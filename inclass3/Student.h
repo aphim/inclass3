9d19f8b25240bc82a71e9c90226005b2ed1a9690
jacky <jacky.yuan@dcmail.ca>
2020-09-30 11:38:05 -0400

    added student class

--- inclass3/Student.h
+++ inclass3/Student.h
@@ -3,36 +3,37 @@
  * @author Jacky Yuan
  */
 
+#pragma once
 
 #ifndef _STUDENT_H
 #define _STUDENT_H
 
 #include "Person.h"
+#include <string>
 
+class Student : public Person {
+public:
 
-class Student: public Person {
-public: 
-    
-/**
- * @param first_name
- * @param last_name
- * @param age
- * @param student_id
- */
-Student(string first_name, string last_name, float age, string student_id);
-    
-string getStudentID();
-    
-/**
- * @param value
- */
-void setStudentID(string value);
-    
-void studies();
-    
-string toString();
-private: 
-    string m_studentID;
+    /**
+     * @param first_name
+     * @param last_name
+     * @param age
+     * @param student_id
+     */
+   Student(const std::string& first_name, const std::string& last_name, float age, std::string student_id);
+
+    std::string getStudentID() const;
+
+    /**
+     * @param value
+     */
+    void setStudentID(const std::string& value);
+    void Studies() const;
+
+   std::string ToString() override;
+
+private:
+    std::string m_studentID;
 };
 
 #endif //_STUDENT_H
