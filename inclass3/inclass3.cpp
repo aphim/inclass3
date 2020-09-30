053de02ed6a3299ef0bdaa5848e46f58d175013e
jacky <jacky.yuan@dcmail.ca>
2020-09-30 11:52:42 -0400

    Added functions in main for testing.

--- inclass3/inclass3.cpp
+++ inclass3/inclass3.cpp
@@ -2,10 +2,29 @@
 //
 
 #include <iostream>
+#include "Person.h"
+#include "Student.h"
+#include "Professor.h"
 
 int main()
 {
-    Person person
-}
+    Person person =Person("Bob", "Smith", 35.6f);
+    person.SaysHello();
+
+    std::cout << person.ToString() << std::endl;
+
+    Student student = Student("Mike", "Jones", 22.0f, "MJ123456789");
+    student.SaysHello();
+    student.Studies();
+
+    std::cout << student.ToString() << std::endl;
+
+    Professor professor = Professor("Andrew", "Hogue", 38.0f, "AH123245367589");
+    professor.SaysHello();
+    professor.Teaches();
+
+    std::cout << professor.ToString() << std::endl;
+
+ }
 
 
